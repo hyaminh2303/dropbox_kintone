d67de5ecb4e34406bf084e0d34ada334bfdcc705
Minh Hy <minhhy@Minhs-MacBook-Air.local>
2021-06-05 22:39:04 +0700

    [WIP]

--- src/view/record_detail/index.tsx
+++ src/view/record_detail/index.tsx
@@ -1,4 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+
 import './style.sass'
 
 export default class RecordDetail extends Component {
@@ -8,9 +11,29 @@ export default class RecordDetail extends Component {
 
   render() {
     return(
-      <div className="record-detail-wrapper">
-        <div className="text">
-          asdad
+      <div className="record-detail-wrapper row-gaia clearFix-cybozu config-position position-relative">
+        <div className="dropbox-wraper">
+          <div className="dropbox-detail-border">
+            <div className="dropbox-breadcrumb">
+              <ul className="breadcrumb">
+                <li>
+                  DropboxForKintone
+                </li>
+                <li><a href="#">pw3soydupwhl[68]</a></li>
+                <li>
+                53
+                </li>
+              </ul>
+            </div>
+            <div>
+              <button className="option-show">
+              <FontAwesomeIcon icon={faPlus} />
+              </button>
+            </div>
+            <div className="dropbox-detail-border">
+              asdadsdas
+            </div>
+          </div>
         </div>
       </div>
     )

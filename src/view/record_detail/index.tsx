bd8ea3514ae058e464bc83475cfa5d7f5536f3a4
Tin Vo <tin.vo@webmtechnology.com>
2021-06-11 14:56:00 +0700

    refactor code

--- src/view/record_detail/index.tsx
+++ src/view/record_detail/index.tsx
@@ -253,7 +253,7 @@ export default class RecordDetail extends Component {
                           variant="contained"
                           startIcon={ <FontAwesomeIcon icon={faTrash} className="fa btn-icon"/> }
                           color="secondary"
-                          >
+                        >
                           Delete
                         </Button>
 

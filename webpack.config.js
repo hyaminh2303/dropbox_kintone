1ad86caff622b6acdb939a65bf090bc30c07bbad
Minh Hy <hyaminh2303@gmail.com>
2021-06-25 08:47:56 +0700

    Update manifest and

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 /* eslint-env node */
 const path = require("path");
 const KintonePlugin = require("@kintone/webpack-plugin-kintone-plugin");
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
 
 const isProduction = process.env.NODE_ENV === "production";
 
@@ -8,9 +9,9 @@ module.exports = {
   mode: isProduction ? "production" : "development",
   devtool: isProduction ? false : "inline-cheap-module-source-map",
   entry: {
-    config: "./src/js/config.ts",
-    desktop: "./src/js/desktop.ts",
-    mobile: "./src/js/mobile.ts",
+    config: "./src/js/config.tsx",
+    desktop: "./src/js/desktop.tsx",
+    mobile: "./src/js/mobile.tsx",
   },
   output: {
     path: path.resolve(__dirname, "plugin", "js"),
@@ -40,9 +41,25 @@ module.exports = {
           ],
         },
       },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
   },
   plugins: [
+    new NodePolyfillPlugin(),
     new KintonePlugin({
       manifestJSONPath: "./plugin/manifest.json",
       privateKeyPath: "./private.ppk",
